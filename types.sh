6d5d7a7e402b7cca0b200a056b07793c2235e487
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-03-14 21:00:09 +0100

    add js types

--- types.sh
+++ types.sh
@@ -3,3 +3,5 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 uv run --with betterproto[compiler] --with grpcio-tools python3 -m grpc_tools.protoc -I . --python_betterproto_out=bond types.proto
+
+pbjs -t json types.proto > bond/frontend/src/types.json
