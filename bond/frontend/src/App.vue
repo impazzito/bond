b27eddd09c63ee2ad9cd183bebfa1edaceaa8f4d
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-03-20 13:04:13 +0100

    Update App.vue

--- bond/frontend/src/App.vue
+++ bond/frontend/src/App.vue
@@ -4,11 +4,10 @@
 
     <div class="flex-grow overflow-y-auto p-4 space-y-4">
         <div v-for="message in data" class="bg-white p-3 rounded-lg shadow-md w-max ">{{ message }}</div>
-
     </div>
 
     <form @submit.prevent=submit class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 p-4 flex items-center">
-        <input type="text" placeholder="Type a message..." class="flex-grow px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+        <input placeholder="Type something..." class="w-full px-3 py-2 rounded-lg bg-white dark:bg-zinc-900 border dark:border-zinc-800 text-sm text-zinc-900 dark:text-zinc-100 placeholder:text-zinc-400 dark:placeholder:text-zinc-600 transition-all duration-200 focus:outline-hidden focus:ring-2 focus:ring-indigo-500/20 border-indigo-500" type="text" v-model="input" ref='field'>
         <button class="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Send</button>
     </form>
 </div>
@@ -21,8 +20,12 @@ import { RouterLink, RouterView } from 'vue-router'
 import { onMounted,ref } from 'vue'
 
 const data = ref([])
+const field = ref(null)
 const input = ref('ciao')
 
+onMounted(async () => {
+    field.value.focus();
+})
 
 async function submit()  {
   console.log('click')
