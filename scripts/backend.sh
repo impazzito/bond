1d337c1118ba973baed778f51d7c6e960254d983
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-03-20 17:23:43 +0100

    fix script path

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")/../bond/frontend"
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 uv run                                       \
     --isolated                               \
