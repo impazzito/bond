5b6264f8a6c6d1207d08ef0c1e6b174a0b28b623
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-03-21 13:06:40 +0100

    use pydantic

--- bond/views/chat.py
+++ bond/views/chat.py
@@ -3,19 +3,21 @@ from collections.abc import AsyncGenerator
 from bond.utils.response import to_streaming_response
 from pydantic import BaseModel
 from fastapi.responses import StreamingResponse
+from bond.views.python import python
+from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+
+from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+from pydantic import BaseModel
+import time, asyncio
 
 
-from agno.agent import Agent
-from agno.models.ollama import Ollama
-from agno.utils.pprint import pprint_run_response
 import time
-
-agent = Agent(
-    model=Ollama(host = 'host.docker.internal'),
-    description="You are an enthusiastic news reporter with a flair for storytelling!",
-    markdown=True
-)
-
 
 
 
@@ -29,9 +31,19 @@ class ChatResponse(BaseModel):
     text: str
 
 
+
+
+agent = Agent(
+    OpenAIModel(
+        model_name='llama3.1',
+        provider=OpenAIProvider(base_url='http://host.docker.internal:11434/v1')
+    ),
+)
+
 async def stream_chat_response(input: ChatInput) -> AsyncGenerator[ChatResponse, None]:
-    async for c in await agent.arun(input.text, stream=True):
-        yield ChatResponse(text=c.content)
+    async with agent.run_stream(input.text) as result:
+        async for message in result.stream_text(delta=True):
+            yield ChatResponse(text=message)
 
 async def chat(input: ChatInput) -> StreamingResponse:
     return to_streaming_response(stream_chat_response, input)
