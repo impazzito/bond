d34d9f841b722ed7707b174bfde307ed2ba18d7d
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-03-21 12:10:35 +0100

    add ollama

--- bond/views/chat.py
+++ bond/views/chat.py
@@ -5,6 +5,22 @@ from pydantic import BaseModel
 from fastapi.responses import StreamingResponse
 
 
+from agno.agent import Agent
+from agno.models.ollama import Ollama
+from agno.utils.pprint import pprint_run_response
+import time
+
+agent = Agent(
+    model=Ollama(host = 'host.docker.internal'),
+    description="You are an enthusiastic news reporter with a flair for storytelling!",
+    markdown=True
+)
+
+
+
+
+
+
 class ChatInput(BaseModel):
     text: str
 
@@ -14,14 +30,8 @@ class ChatResponse(BaseModel):
 
 
 async def stream_chat_response(input: ChatInput) -> AsyncGenerator[ChatResponse, None]:
-    for msg in [
-        ChatResponse(text="Hello {}\n".format(input.text)),
-        ChatResponse(text="Upper {}\n".format(input.text.upper())),
-        ChatResponse(text="Length: {}\n".format(len(input.text))),
-    ]:
-        yield msg
-        await asyncio.sleep(0.2)
-
+    async for c in await agent.arun(input.text, stream=True):
+        yield ChatResponse(text=c.content)
 
 async def chat(input: ChatInput) -> StreamingResponse:
     return to_streaming_response(stream_chat_response, input)
